58c9609c3cf6fe80bd4502240eb6e5d70b6ef57b
filesmuggler <krzste09@outlook.com>
2018-01-13 19:01:55 +0100

    first code

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+void sleep(uint8_t millisec){
+    while(millisec){
+        _delay_ms(1);
+        millisec--;
+    }
+}
+
+main(){
+    DDRC |=1<<PB7;
+    while(1){
+        PORTC &= ~(1<<PB7);
+        sleep(100);
+
+        PORTC !=(1<<PB7);
+        sleep(100);
+    }
+}
